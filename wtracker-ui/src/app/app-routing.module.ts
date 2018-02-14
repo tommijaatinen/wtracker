b4ae3959f4ada36e22e9b1e4a2bf78eb9e711fa6
tommijaatinen <tommi.jaatinen@gmail.com>
2018-02-14 09:03:48 +0200

    Move page-not-found component to page-not-found folder

--- wtracker-ui/src/app/app-routing.module.ts
+++ wtracker-ui/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { WodComponent } from './wod/wod.component';
 import { WodResultsComponent } from './wod-results/wod-results.component';
 import { AthleteComponent } from './athlete/athlete.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     { path: 'wtracker/wods', component: WodComponent, pathMatch: 'full' },
